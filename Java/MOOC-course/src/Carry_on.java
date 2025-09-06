ab10a91c1ffd4c1d7b1d9a19d33086231b4716aa
Abhishek Rajvir <abhishekrajvir66@gmail.com>
2025-09-06 13:36:16 +0530

    Added Carry_on

---
+++ Java/MOOC-course/src/Carry_on.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Carry_on{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) { 
+            System.out.println("Shall we carry on?: ");
+            if ( (sc.next().toLowerCase()).equals("no") ) { break; }
+            else {  }
+        }
+    }
+}

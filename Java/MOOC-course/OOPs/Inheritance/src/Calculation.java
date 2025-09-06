70146e95707e8ca39c1046a67cc721c629b90b20
Abhishek Rajvir <abhishekrajvir66@gmail.com>
2025-09-06 23:49:53 +0530

    Add OOP\Inheritance Calculation

---
+++ Java/MOOC-course/OOPs/Inheritance/src/Calculation.java
@@ -0,0 +1,35 @@
+// import Scanner
+import java.util.Scanner;
+
+class Calculation{
+    private int x;
+    private int y;
+    Calculation(){}
+    Calculation(int x,int y) { 
+        this.x = x;
+        this.y = y;
+    }
+
+    public int Add(){
+        return x+y;
+    }
+    public static int Add(int x,int y){
+        return x+y;
+    }
+    public int Sub(){
+        return x-y;
+    }
+    public static int Sub(int x,int y){
+        return x-y;
+    }
+}
+
+// create class for main func
+class MyCalculation extends Calculation{
+    // main function
+    public static void main(String[] args){
+        Calculation c1 = new Calculation(3,5);
+        System.out.println("Addition: " + c1.Add() );
+        System.out.println("Substraction: " + c1.Sub() );
+    }
+}
